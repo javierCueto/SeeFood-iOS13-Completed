40e410f182f6ffdd6fee139ef3250824260a7176
Javier <javelie@outlook.com>
2019-12-09 20:59:45 -0700

    review

--- SeeFood-CoreML/ViewController.swift
+++ SeeFood-CoreML/ViewController.swift
@@ -79,7 +79,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     @IBAction func cameraTapped(_ sender: Any) {
-        
+//        if a wish i use the library
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
         present(imagePicker, animated: true, completion: nil)
